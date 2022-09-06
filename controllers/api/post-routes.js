ffcfac6e26a45be52f09733cd0ae2a1499dda92c
Alka sah <alkasah485@gmail.com>
2022-09-06 02:25:24 -0700

    updation in api files

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { User,Post,Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const { User,Post,Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
