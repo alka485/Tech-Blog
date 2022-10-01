fbe289c3750802b1466f3f14fcd268de68d9da2a
alkasah <alkasah485@gmail.com>
2022-09-30 20:55:34 -0700

    updation in user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,12 +6,10 @@ router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
-      //email: req.body.email,
       password: req.body.password,
     });
 
     req.session.save(() => {
-      // req.session.user_id = dbUserData.id;
       req.session.logged_in = true;
 
       res.status(200).json(dbUserData);
@@ -21,5 +19,45 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+    req.session.save(() => {
+        req.session.logged_in = true;
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get('/', async (req, res) => {
+    try {
+            const userData = await User.findAll();
+      
+      const users = userData.map((user) => user.toJSON());
+      //res.render('test', { users })
+  
+        res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
 
 module.exports = router;
